ce3c61cd88ebe5f07dc8d3a48665cf894b212459
nfaguirrec <nfaguirrec@gmail.com>
2016-08-03 19:38:10 +0200

    isConnected ahora tienen la posibilidad de cambiar la tolerancia al hacer enlaces
    Former-commit-id: 95e544f0e927817cce68f85e5df774ffcbd3ff87

--- examples/molecule.isConnected.f90
+++ examples/molecule.isConnected.f90
@@ -9,19 +9,26 @@ program main
 	
 	character(1000) :: sBuffer
 	type(String) :: iFileName
+	real(8) :: alpha
 	type(Molecule) :: mol
-	type(Matrix) :: Im
 	
 	if( command_argument_count() < 1 ) then
-		write(*,*) "## ERROR ## Number of Parameter == 1"
+		write(*,*) "usage: molecule.isConnected geomFile [alpha]"
+		write(*,*) "                                       1.0  "
 		stop
 	end if
 	
 	call get_command_argument( 1, sBuffer )
 	iFileName = sBuffer
 	
+	alpha = 1.0_8
+	if( command_argument_count() > 1 ) then
+		call get_command_argument( 2, sBuffer )
+		alpha = FString_toReal( sBuffer )
+	end if
+	
 	call mol.init( iFileName.fstr )
 	
-	call mol.buildGraph()
+	call mol.buildGraph( alpha=alpha )
 	write(*,*) mol.molGraph.isConnected()
 end program main
