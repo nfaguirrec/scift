6ed20dbc7774c15c6c769ba2b42ea82a7cb26966
nfaguirrec <nfaguirrec@gmail.com>
2017-08-26 23:38:21 -0600

    Headers and license updated

--- src/unstable/StringIntegerHistogramMap.f90
+++ src/unstable/StringIntegerHistogramMap.f90
@@ -1,27 +1,38 @@
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!
-!!  This file is part of scift (Scientific Fortran Tools).
-!!  Copyright (C) by authors (2012-2013)
-!!  
-!!  Authors (alphabetic order):
-!!    * Aguirre N.F. (nfaguirrec@gmail.com)  (2012-2014)
-!!  
-!!  Contributors (alphabetic order):
-!!  
-!!  Redistribution and use in source and binary forms, with or
-!!  without modification, are permitted provided that the
-!!  following conditions are met:
-!!  
-!!   * Redistributions of binary or source code must retain
-!!     the above copyright notice and this list of conditions
-!!     and/or other materials provided with the distribution.
-!!   * All advertising materials mentioning features or use of
-!!     this software must display the following acknowledgement:
-!!     
-!!     This product includes software from scift
-!!     (Scientific Fortran Tools) project and its contributors.
-!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!                                                                                   !!
+!!  This file is part of SciFT project                                               !!
+!!  Copyright (c) 2012-2013 Nestor F. Aguirre (nfaguirrec@gmail.com)                 !!
+!!                                                                                   !!
+!!  Redistribution and use in source and binary forms, with or without               !!
+!!  modification, are permitted provided that the following conditions are met:      !!
+!!                                                                                   !!
+!!  1. Redistributions of source code must retain the above copyright notice, this   !!
+!!     list of conditions and the following disclaimer.                              !!
+!!  2. Redistributions in binary form must reproduce the above copyright notice,     !!
+!!     this list of conditions and the following disclaimer in the documentation     !!
+!!     and/or other materials provided with the distribution.                        !!
+!!  3. Neither the name of the copyright holders nor the names of its contributors   !!
+!!     may be used to endorse or promote products derived from this software         !!
+!!     without specific prior written permission.                                    !!
+!!                                                                                   !!
+!!  The copyright holders provide no reassurances that the source code provided      !!
+!!  does not infringe any patent, copyright, or any other intellectual property      !!
+!!  rights of third parties.  The copyright holders disclaim any liability to any    !!
+!!  recipient for claims brought against recipient by any third party for            !!
+!!  infringement of that parties intellectual property rights.                       !!
+!!                                                                                   !!
+!!  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND  !!
+!!  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED    !!
+!!  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE           !!
+!!  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR  !!
+!!  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES   !!
+!!  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;     !!
+!!  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND      !!
+!!  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT       !!
+!!  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS    !!
+!!  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                     !!
+!!                                                                                   !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 module StringIntegerHistogramMap_
 	use IOStream_
