f3c3e22e751a13e27e2689256021836ef78e106e
nfaguirrec <nfaguirrec@gmail.com>
2018-06-09 18:35:10 -0600

    New command molecule.rms

---
+++ examples/molecule.rms.f90
@@ -0,0 +1,82 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!                                                                                   !!
+!!  This file is part of SciFT project                                               !!
+!!  Copyright (c) 2011-2016 Nestor F. Aguirre (nfaguirrec@gmail.com)                 !!
+!!                                                                                   !!
+!!  Redistribution and use in source and binary forms, with or without               !!
+!!  modification, are permitted provided that the following conditions are met:      !!
+!!                                                                                   !!
+!!  1. Redistributions of source code must retain the above copyright notice, this   !!
+!!     list of conditions and the following disclaimer.                              !!
+!!  2. Redistributions in binary form must reproduce the above copyright notice,     !!
+!!     this list of conditions and the following disclaimer in the documentation     !!
+!!     and/or other materials provided with the distribution.                        !!
+!!  3. Neither the name of the copyright holders nor the names of its contributors   !!
+!!     may be used to endorse or promote products derived from this software         !!
+!!     without specific prior written permission.                                    !!
+!!                                                                                   !!
+!!  The copyright holders provide no reassurances that the source code provided      !!
+!!  does not infringe any patent, copyright, or any other intellectual property      !!
+!!  rights of third parties.  The copyright holders disclaim any liability to any    !!
+!!  recipient for claims brought against recipient by any third party for            !!
+!!  infringement of that parties intellectual property rights.                       !!
+!!                                                                                   !!
+!!  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND  !!
+!!  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED    !!
+!!  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE           !!
+!!  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR  !!
+!!  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES   !!
+!!  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;     !!
+!!  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND      !!
+!!  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT       !!
+!!  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS    !!
+!!  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                     !!
+!!                                                                                   !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!>
+!! @brief Test program
+!!
+program main
+	use UnitsConverter_
+	use String_
+	use Molecule_
+	implicit none
+	
+	character(1000) :: sBuffer
+	type(String) :: iFileName1, iFileName2
+	type(Molecule) :: mol1, mol2
+	real(8) :: rms
+	integer :: i, j
+	
+	if( command_argument_count() < 2 ) then
+		write(*,*) "Usage: molecule.compare file1 file2"
+		write(*,*) "                                   "
+		stop
+	end if
+	
+	call get_command_argument( 1, sBuffer )
+	iFileName1 = sBuffer
+	
+	call get_command_argument( 2, sBuffer )
+	iFileName2 = sBuffer
+	
+	call mol1.load( iFileName1.fstr )
+	call mol2.load( iFileName2.fstr )
+	
+	if( mol1.nAtoms() /= mol2.nAtoms() ) then
+		stop "### ERROR ### Mol1 and Mol2 have not the same number of atoms"
+	end if
+	
+	rms = 0.0_8
+	do i=1,mol1.nAtoms()
+		if( trim(mol1.atoms(i).symbol) /= trim(mol2.atoms(i).symbol) ) then
+			stop "### ERROR ### Symbols in Mol1 are incompatible with Mol2"
+		end if
+		
+		rms = rms + sum( ( mol1.atoms(i).r - mol2.atoms(i).r )**2 )
+	end do
+	
+	write(*,"(F15.8)") sqrt(rms)/angs
+	
+end program main
